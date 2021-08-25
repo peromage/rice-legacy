2f27665bc34c6edc065885f0c42bbdd6d8226f79
peromage <peromage@outlook.com>
2021-08-24 20:27:10 -0400

    Import rice configurations

---
+++ ripwsh/base/mod-env.ps1
@@ -0,0 +1,12 @@
+<###############################################################################
+
+.SYNOPSIS
+Environment variable setup for the session
+
+Created by peromage 2021/02/24
+Last modified 2021/03/05
+
+###############################################################################>
+
+$env:EDITOR = "vim"
+$env:SHELL = "pwsh"
