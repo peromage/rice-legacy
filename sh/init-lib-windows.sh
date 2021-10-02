5f728c053b183d24c0c71725e1bb87894c4511ad
peromage <peromage@outlook.com>
2021-10-02 16:44:16 -0400

    Update bash init files for Windows

--- sh/init-lib-windows.sh
+++ sh/init-lib-windows.sh
@@ -1,4 +1,14 @@
-### init-windows.sh -- Shell in Windows
+### init-lib-windows.sh -- Shell in Windows
+
+##------------------------------------------------------------------------------
+## For Windows only e.g. mysys, cygwin
+##------------------------------------------------------------------------------
+
+case "$OS" in
+    *Windows*) ;;
+    *windows*) ;;
+    *) return;;
+esac
 
 alias win_msys_update="pacman --needed -S bash pacman pacman-mirrors msys2-runtime"
 alias win_cygwin_install="cygwin-setup --no-admin --no-desktop --no-shortcuts --no-startmenu"
