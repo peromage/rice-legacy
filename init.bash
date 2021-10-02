70dcb6826be115059fd331c6bcc24f3013a8ecf6
peromage <peromage@outlook.com>
2021-10-02 10:43:29 -0400

    Update bash init files

--- init.bash
+++ init.bash
@@ -1,4 +1,4 @@
-#!/usr/bin/bash
+#!/bin/bash
 ### init.sh -- Bootstrap for Bash
 
 ## Pre-checks
@@ -7,7 +7,7 @@ case "$-" in
     *i*) ;;
     *) return;;
 esac
-## Is shell bash
+## Bash
 [ -z "$BASH_VERSION" ] && return
 ## No nested loading
 [ -n "$rice_loaded" ] && return
@@ -19,17 +19,15 @@ rice_scripts="$rice_home/scripts"
 rice_local="$rice_home/local.bash"
 export PATH="$PATH:$rice_scripts"
 rice_inc() {
-    _tmp="$1" && shift
-    . "$rice_home/shell/$_tmp.sh" $@
+    local module="$1" && shift
+    . "$rice_home/sh/$module.sh" $@
 }
 
 ## Bootstrap
-rice_inc init-cmd
+rice_inc init-aliases
 rice_inc init-windows
 rice_inc theme-normal
 rice_inc plugin-zlua
 
 ## Wrapup
-if [ -f "$rice_local" ]; then
-    . "$rice_local"
-fi
+[ -f "$rice_local" ] && . "$rice_local"
