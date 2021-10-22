e6d33782bf33f4d1aebc9abb08419d241d1528e4
peromage <peromage@outlook.com>
2021-10-21 22:33:11 -0400

    Adjust bash file hierarchy

--- init.bash
+++ init.bash
@@ -1,45 +1,31 @@
 #!/bin/bash
-### init.sh -- Bootstrap for Bash
+### init.bash -- Bootstrap for Bash
 
 ##------------------------------------------------------------------------------
-## Pre-checks
+## Prerequisites
 ##------------------------------------------------------------------------------
 
-## interactive sessions
+## Interactive sessions
 case "$-" in
     *i*) ;;
     *) return;;
 esac
+
 ## Bash
 [ -z "$BASH_VERSION" ] && return
-## No nested loading
-[ -n "$rice_loaded" ] && return
-
-##------------------------------------------------------------------------------
-## Environment initialization
-##------------------------------------------------------------------------------
-
-rice_loaded=1
-rice_home_dir=$(dirname $(realpath "$BASH_SOURCE"))
-rice_scripts_dir="$rice_home_dir/scripts"
-rice_local_file="$rice_home_dir/local.bash"
-export PATH="$PATH:$rice_scripts_dir"
-
-rice_inc() {
-    local module="$1" && shift
-    . "$rice_home_dir/sh/$module.sh" $@
-}
 
 ##------------------------------------------------------------------------------
 ## Bootstrap
 ##------------------------------------------------------------------------------
 
-rice_inc init-aliases
-rice_inc theme-normal
-rice_inc plugin-zlua
+## Modules
+. $(dirname $(realpath "$BASH_SOURCE"))/bash/init-base.bash
+rice_include theme-normal
+rice_include plugin-zlua
 
-##------------------------------------------------------------------------------
-## Wrapup
-##------------------------------------------------------------------------------
+## PATH
+export PATH="$PATH:$rice_scripts_dir"
 
-[ -f "$rice_local_file" ] && . "$rice_local_file"
+## Local config
+rice_source_if_exists "$rice_home_dir/aliases.sh"
+rice_source_if_exists "$rice_home_dir/local.bash"
