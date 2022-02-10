08c1b1b03f818d8cfc42849292324ba90e06618c
peromage <peromage@outlook.com>
2022-02-09 23:40:40 -0500

    Update Bash init scripts

--- init.bash
+++ init.bash
@@ -1,31 +1,45 @@
 #!/bin/bash
 ### init.bash -- Bootstrap for Bash
 
-##------------------------------------------------------------------------------
+################################################################################
 ## Prerequisites
-##------------------------------------------------------------------------------
+################################################################################
 
-## Interactive sessions
+## Make sure sourced by Bash
+[[ -z $BASH_VERSION ]] && return
+
+## Only sourced once
+[[ -n $LOADED_RICE_BASH ]] && return
+LOADED_RICE_BASH=1
+
+## Only sourced in interactive sessions
 case "$-" in
     *i*) ;;
     *) return;;
 esac
 
-## Bash
-[ -z "$BASH_VERSION" ] && return
+## Directory where this script is
+rice_home_dir=$(dirname $(realpath "$BASH_SOURCE"))
 
-##------------------------------------------------------------------------------
-## Bootstrap
-##------------------------------------------------------------------------------
-
-## Modules
-. $(dirname $(realpath "$BASH_SOURCE"))/bash/init-base.bash
-rice_include theme-normal
-rice_include plugin-zlua
+rice_include() {
+    [[ ! -d $rice_home_dir ]] && return
+    local file=$rice_home_dir/$1 && shift
+    #local skip_not_exists=$1 && shift
+    #[[ -n $skip_not_exists && ! -f $file ]] && return
+    [[ ! -f $file ]] && return
+    source $file $@
+}
 
 ## PATH
-export PATH="$PATH:$rice_scripts_dir"
+PATH=$PATH:$rice_home_dir/scripts
 
-## Local config
-rice_source_if_exists "$rice_home_dir/aliases.sh"
-rice_source_if_exists "$rice_home_dir/local.bash"
+################################################################################
+## Load other files
+################################################################################
+
+rice_include bash/init-env.bash
+rice_include bash/init-aliases.bash
+rice_include bash/init-aliases-win.bash
+rice_include bash/plugin-zlua.bash
+rice_include bash/theme-normal.bash
+rice_include local.bash
